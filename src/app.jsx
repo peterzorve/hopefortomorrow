bed5209e8a8cc452f1b19551114cf6eae1f617f3
Peter Zorve <98400766+peterzorve@users.noreply.github.com>
2024-08-26 02:13:10 +0300

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -16,7 +16,7 @@ import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router basename="/hopefortomorrow/">
       <Navbar/>
         <div className="container">
           <Routes>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/hopefortomorrow/',
   // base: '/',
   plugins: [preact()],
 })
